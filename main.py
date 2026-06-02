5d6eb6327c2fb9b435ab2b03bbae987e90c46a9e
malavika-design <malavika@everstage.com>
2026-06-02 21:01:05 +0530

    Update main.py

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from fastapi.middleware.cors import CORSMiddleware
 from fastapi.responses import Response
 from rembg import remove
 import uvicorn
+import os
 
 app = FastAPI()
 
@@ -24,4 +25,5 @@ async def remove_bg(file: UploadFile = File(...)):
     return Response(content=output_bytes, media_type="image/png")
 
 if __name__ == "__main__":
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+    port = int(os.environ.get("PORT", 8000))
+    uvicorn.run(app, host="0.0.0.0", port=port)
